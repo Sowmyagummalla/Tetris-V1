f26d25fd7d5b76267ac7ec5ac6d8ee5f85499f9f
Sowmyagummalla <sowmyareddygummalla@gmail.com>
2024-08-12 12:18:47 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "projects3bucket25" # Replace with your actual S3 bucket name
+    bucket = "awsbucketsow25" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
